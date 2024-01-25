ad55774c3befe8153f94ec3e4adf283e42ff697e
abhilashlegend <abhilash456a@live.com>
2024-01-25 19:39:24 +0530

    Added bootstrap template

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const shopRoutes = require("./routes/home");
 const bodyParser = require("body-parser");
+const path = require('path');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+
 
 
 app.use(shopRoutes);
