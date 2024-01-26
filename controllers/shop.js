ad316150495b3f55fd2fcc037c8101e36ecdfb3a
abhilashlegend <abhilash456a@live.com>
2024-01-26 13:27:36 +0530

    Added page title

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,8 @@
 
 exports.index = (req, res, next) => {
-    res.render("index.ejs", { pageTitle: "Home Page" });
+    res.render("index.ejs", { pageTitle: "Home Page", path: "/" });
 }
 
 exports.about = (req, res, next) => {
-    res.render("about.ejs", {pageTitle: "About Page" });
+    res.render("about.ejs", {pageTitle: "About Page", path: "/about" });
 }
